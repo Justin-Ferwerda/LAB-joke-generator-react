1f1808b77aa38c9a45f8ac78181f40dd77e585e2
Justin Ferwerda <justin.ferwerda@gmail.com>
2022-07-12 19:55:32 -0500

    refactored GetJokeButton

--- components/GetJokeButton.js
+++ components/GetJokeButton.js
@@ -2,16 +2,24 @@ import { useState } from 'react';
 import getJoke from '../api/jokeData';
 
 const GetJokeButton = () => {
-  const [joke, setJoke] = useState();
+  const [joke, setJoke] = useState({});
+  const [buttonText, setButtonText] = useState('Get A Joke');
 
   const jokeOnDom = () => {
-    getJoke().then((jokeObject) => setJoke(jokeObject.setup));
+    if (buttonText === 'Get A Joke' || buttonText === 'Get Another Joke') {
+      getJoke().then((jokeObject) => setJoke(jokeObject)).then(() => {
+        setButtonText('Get Punchline');
+      });
+    } else {
+      setButtonText('Get Another Joke');
+    }
   };
 
   return (
     <>
-      <h2>{joke}</h2>
-      <button type="button" onClick={jokeOnDom}>{joke ? 'Get Punchline' : 'Get a Joke'}</button>
+      <h2>{ buttonText === 'Get a Joke' ? '' : joke.setup}</h2>
+      <h5>{ buttonText === 'Get Another Joke' ? joke.delivery : ''}</h5>
+      <button type="button" onClick={jokeOnDom}>{buttonText}</button>
     </>
   );
 };
